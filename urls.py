007ae99061144c2cee0056ccaabb10bf774281a5
John Debs <john@allforlocal.com>
2010-08-03 21:34:32 -0400

    Renamed project to basicproject, added basic URLconf, and added an index page.

--- urls.py
+++ urls.py
@@ -5,5 +5,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'basicproject.views.index', name="home"),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('',
+        (r'^site_media/', include('staticfiles.urls')),
+    )
