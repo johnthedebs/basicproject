cb934684724168cc7a5e05cf269d0ef3cae68016
John Debs <john@allforlocal.com>
2010-08-25 15:57:54 -0400

    Set full path for database

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'basicproject.db',                   # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT, 'basicproject.db'),                   # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
