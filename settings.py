7fbecb41ffb77626f34d3a1fdaaa63835eb56afa
John Debs <johnthedebs@gmail.com>
2011-01-03 17:21:55 -0500

    A few more tweaks

--- settings.py
+++ settings.py
@@ -7,7 +7,6 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-SERVE_MEDIA = DEBUG
 
 ADMINS = (
     # ("Your Name", "your_email@domain.com"),
--- static_media/config.rb
+++ static_media/config.rb
@@ -8,7 +8,7 @@ line_comments = false
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions:
 relative_assets = true
