dc5d54e8ccbb1f45c593ee60e7db051ecca67de7
John Debs <johnthedebs@gmail.com>
2011-01-19 18:02:54 -0500

    Removed unnecessary comments

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
-# Django settings for basicproject project.
-
 import os.path
 import posixpath
 
@@ -94,8 +92,6 @@ DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": False,
 }
 
-# local_settings.py can be used to override environment-specific settings
-# like database and email that differ between development and production.
 try:
     from local_settings import *
 except ImportError:
