2bc630c12ba57f02508d6ee7a6cba9e684adbb4e
John Debs <johnthedebs@gmail.com>
2011-11-10 03:20:22 -0500

    Updated everything, switched project to new layout

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ This Is
 A Django skeleton project set up the way I like to roll. The
 requirements file will install Django with the following apps:
 
+    django-compressor
     django-debug-toolbar
     django-extensions
+    django-uni-form
 
 as well as some supporting packages. See `requirements.txt` for a more
 complete list.
@@ -20,7 +22,7 @@ From your terminal:
 
     git clone git@github.com:johnthedebs/basicproject.git project_name
     cd project_name
-    mkvirtualenv project_name --no-site-packages # optional
+    mkvirtualenv project_name --no-site-packages
     pip install -r requirements.txt
     python manage.py syncdb
     python manage.py runserver
--- manage.py
+++ manage.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python
 import os, sys
 
-from django.conf import settings
-from django.core.management import setup_environ, execute_from_command_line
-
-try:
-    import settings as settings_mod # Assumed to be in the same directory.
-except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
-
-# setup the environment before we start accessing things in the settings.
-setup_environ(settings_mod)
-
-sys.path.insert(0, os.path.join(settings.PROJECT_ROOT, 'apps'))
-
 if __name__ == "__main__":
-    execute_from_command_line()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
