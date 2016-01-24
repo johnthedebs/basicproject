360238a92de298dd51a892189d9ed7e5759ec27f
John Debs <johnthedebs@gmail.com>
2016-01-24 18:34:32 -0500

    Add very simple django-rest-framework ViewSet for User model

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,20 @@ from django.conf.urls                import patterns, include, url
 from django.contrib                  import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from .views import app
+from rest_framework import routers
+
+from .views import app, UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register("users", UserViewSet)
 
 
 urlpatterns = patterns("",
     url(r"^$", app, name="app"),
 
     url(r"^admin/", include(admin.site.urls)),
+    url(r"^api/", include(router.urls)),
     url(r"^django-rq/", include("django_rq.urls")),
 
     # This needs to go at the end since it captures anything
--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts    import render
+
+from rest_framework import viewsets
+
+from .serializers import UserSerializer
+
+
+User = get_user_model()
 
 
 def app(request, path=None):
     return render(request, "app.html", {})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
